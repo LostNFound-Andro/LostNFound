ef436e3ff91d0e8ad9640a837360456c469b4657
simsarulhaqv <simsarulhaqv@gmail.com>
2016-04-03 09:56:03 +0530

    Create Resolve activity

---
+++ app/src/main/java/com/example/lostnfound/lostnfound/Resolve.java
@@ -0,0 +1,16 @@
+package com.example.lostnfound.lostnfound;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by simsar on 1/4/16.
+ */
+
+public class  Resolve extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.resolve);
+    }
+}
