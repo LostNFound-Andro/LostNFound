42a7ec8b467e9d575310346802ff85bd923fd48e
ahamedhak <ahamedhak@gmail.com>
2016-04-14 17:46:25 +0530

    add unsubscribe button

--- app/src/main/java/com/example/sel/lostfound/SubscribeAdapter.java
+++ app/src/main/java/com/example/sel/lostfound/SubscribeAdapter.java
@@ -1,15 +1,31 @@
 package com.example.sel.lostfound;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by achu on 14-04-2016.
@@ -49,7 +65,7 @@ public class SubscribeAdapter extends ArrayAdapter<Category> {
             subscribeHolder = (SubscribeHolder) row.getTag();
         }
 
-        Category category = (Category) this.getItem(position);
+        final Category category = (Category) this.getItem(position);
 
         subscribeHolder.tx_cat.setText(category.getCategory());
 
@@ -57,12 +73,79 @@ public class SubscribeAdapter extends ArrayAdapter<Category> {
         Button unsub_button = (Button) row.findViewById(R.id.unsub_button);
         unsub_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+//                Toast.makeText(context,category.getCid(), Toast.LENGTH_LONG).show();
 
-//                Toast.makeText(context,s, Toast.LENGTH_LONG).show();
+                Toast.makeText(context,"Unsubscribe",Toast.LENGTH_LONG).show();
+
+                new AsyncTask<Void, Void, Void>() {
+
+                    @Override
+                    protected Void doInBackground(Void... params) {
+                        String email = MainActivity.userEmail;
+                        String checkUrl = "http://52.38.30.3/deletesubscription.php";
+                        try {
+                            URL url = new URL(checkUrl);
+                            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                            httpURLConnection.setReadTimeout(10000);
+                            httpURLConnection.setConnectTimeout(15000);
+                            httpURLConnection.setRequestMethod("POST");
+                            httpURLConnection.setDoOutput(true);
+                            httpURLConnection.setDoInput(true);
+                            OutputStream OS = httpURLConnection.getOutputStream();
+                            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(OS, "UTF-8"));
+                            ContentValues data = new ContentValues();
+                            data.put("email", email);
+                            data.put("catid",category.getCid());
+                            bufferedWriter.write(getQuery(data));
+                            bufferedWriter.flush();
+                            bufferedWriter.close();
+                            OS.close();
+
+                            InputStream IS = httpURLConnection.getInputStream();
+                            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(IS,"iso-8859-1"));
+                            String response = "";
+                            String line = "";
+                            while ((line = bufferedReader.readLine())!=null)
+                            {
+                                response+= line;
+                            }
+                            bufferedReader.close();
+                            IS.close();
+                            httpURLConnection.disconnect();
+                        } catch (MalformedURLException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        return null;
+                    }
+                }.execute();
             }
         });
 
+       // list.remove(position);
+       // notifyDataSetChanged();
+
         return row;
+    }
+    private String getQuery(ContentValues params) throws UnsupportedEncodingException
+    {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+
+        for (Map.Entry<String, Object> entry : params.valueSet())
+        {
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue().toString(), "UTF-8"));
+        }
+
+        return result.toString();
     }
 
     static class SubscribeHolder{
