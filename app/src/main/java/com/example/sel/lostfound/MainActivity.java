f281a7d69f9f0e3f704d510cc33dea171d105467
devrogar <bablu.rohit@hotmail.com>
2016-04-15 19:05:51 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/sel/lostfound/MainActivity.java
+++ app/src/main/java/com/example/sel/lostfound/MainActivity.java
@@ -62,7 +62,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nitc_email = (EditText) findViewById(R.id.emailText);
         mStatusTextView = (TextView) findViewById(R.id.status);
+        proceedButton = (Button) findViewById(R.id.proceedButton);
         signinButton = (com.google.android.gms.common.SignInButton) findViewById(R.id.sign_in_button);
         continueButton = (Button)findViewById(R.id.continueButton);
         continueButton.setVisibility(View.INVISIBLE);
@@ -156,9 +158,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             userName = acct.getDisplayName();
             userEmail = acct.getEmail();
             userImage = acct.getPhotoUrl();
-            if (userEmail.matches(emailPattern))
+            if (userEmail.matches("devrogar@gmail.com"))
             {
-                Toast.makeText(this,"Signed in as "+userEmail,Toast.LENGTH_LONG).show();
                 finish();
                 startActivity(new Intent(this, FeedActivity.class));
             }
